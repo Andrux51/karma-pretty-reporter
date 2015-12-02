ddfa3e31f9c8cd5d5a2b820117c969dd0702c6d6
Andy Martin <andymartin51@gmail.com>
2015-12-02 13:01:58 -0700

    add some debug stuff, implement karma reporter setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Karma Pretty Reporter
+
+Inspired by [karma-htmlfile-reporter](https://github.com/matthias-schuetz/karma-htmlfile-reporter)
--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-exports.printMsg = function() {
-    console.log('printmsg');
+var xmlbuilder = require('xmlbuilder');
+
+var prettyReporter = function(baseReporterDecorator, config, logger, helper, formatError) {
+    var outputFile = config.prettyReporter.outputFile;
+    var html, body;
+
+    baseReporterDecorator(this);
+
+    this.onRunStart = function(browsers) {
+        html = builder.create('html');
+        html.doctype();
+    };
+
+    this.onBrowserStart = function(browser) {
+        // NOTE: each browser is starting
+        this.write('browser detected: ' + browser.name + '/r/n')
+    };
+
+    this.onSpecComplete = function(browser, result) {
+        this.write(result.success + '/r/n');
+    };
+
+    this.onBrowserComplete = function(browser) {
+        // NOTE: each browser has finished
+    };
+
+    this.onRunComplete = function(browsers, results) {
+        // NOTE: all browsers have finished
+        this.write(html + '/r/n');
+    };
+
+    this.onExit = function(callback) {
+        // NOTE: before exiting Karma
+    };
+};
+
+prettyReporter.$inject = ['baseReporterDecorator', 'config', 'logger', 'helper', 'formatError'];
+
+module.exports = {
+    'reporter:pretty': ['type': prettyReporter]
 }
